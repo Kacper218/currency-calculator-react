41f716251adc30a95349ffc305a772acf46a0ce0
Kacper Przybyszewski <kacper.przybyszewskiiii@gmail.com>
2021-05-14 17:15:23 +0200

    pick currency and input amount are now functional

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,33 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./style.css";
 
-const Form = () => {
-    return (
+const Form = ({ options }) => {
 
-        <form className="form">
-            <fieldset className="form__fieldset">
-                <legend className="form__legend">Kalkulator walutowy</legend>
-                <p>
-                    <span className="form__labelText">Wybierz walutę: </span><select className="form__field" name="currency">
-                        <option value="euro">Euro</option>
-                        <option value="ruda">Magiczna Ruda</option>
-                    </select>
-                </p>
-                <p>
-                    <label>
-                        <span className="form__labelText">Podaj kwotę do przeliczenia w zł: </span><input required className="form__field" type="number" min="0.01" step="0.01" />
-                    </label>
-                </p>
-                <p>
-                    <label>
-                        <span className="form__labelText">Podaj aktualny kurs euro:</span> <input required className="form__field" type="number" min="0.01" step="0.01" value="4.51" /> </label>
-                </p>
-            </fieldset>
-            <p>
-                <button className="form__button">Oblicz!</button>
-            </p>
-        </form>
-    );
+  const [x, setX] = useState(1);
+  const [amount, setAmount] = useState(0);
+  const [converter, setConverter] = useState(4.51);
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const onChangeSelect = () => {
+    if (x === 1) {
+      setConverter(converter => converter = 21.37)
+      setX(x => x + 1)
+    } else {
+      setConverter(converter => converter = 4.51)
+      setX(x => x - 1)
+    }
+  }
+
+  return (
+
+    <form
+      className="form"
+      onSubmit={onFormSubmit}
+    >
+      <fieldset className="form__fieldset">
+        <legend className="form__legend">Kalkulator walutowy</legend>
+        <p>
+          <span className="form__labelText">
+            Wybierz walutę:
+            </span>
+          <select
+            className="form__field"
+            name="currency"
+            onChange={onChangeSelect}
+          >
+            {options.map((option) => (
+              <option
+                key={option.id}
+                value={option.value}
+              >
+                {option.label}
+              </option>
+            ))}
+          </select>
+        </p>
+        <p>
+          <label>
+            <span className="form__labelText">
+              Podaj kwotę do przeliczenia w zł:
+              </span>
+            <input
+              required
+              className="form__field"
+              type="number"
+              min="0.01"
+              step="0.01"
+              value={amount}
+              onChange={({ target }) => setAmount(target.value)}
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            <span className="form__labelText">
+              Podaj aktualny kurs euro:
+              </span>
+            <input
+              required
+              className="form__field"
+              type="number"
+              min="0.01"
+              step="0.01"
+              value={converter}
+            />
+          </label>
+        </p>
+      </fieldset>
+      <p>
+        <button className="form__button">Oblicz!</button>
+      </p>
+    </form>
+  );
 };
 
 export default Form;
