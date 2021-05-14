aff362d620941cd23d0a5e97926f1956afb3059a
Kacper Przybyszewski <kacper.przybyszewskiiii@gmail.com>
2021-05-14 14:56:08 +0200

    basic sketch witch background, font and regular form

--- src/App.js
+++ src/App.js
@@ -2,24 +2,44 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<body className="body">
+			<div className="container">
+				<main>
+
+					<form className="form">
+						<fieldset className="form__fieldset">
+							<legend className="form__legend">Kalkulator walutowy</legend>
+							<p>
+								<span className="form__labelText">Wybierz walutę: </span><select className="form__field" name="currency">
+									<option value="euro">Euro</option>
+									<option value="ruda">Magiczna Ruda</option>
+								</select>
+							</p>
+							<p>
+								<label>
+									<span className="form__labelText">Podaj kwotę do przeliczenia w zł: </span><input required className="form__field" type="number" min="0.01" step="0.01" />
+								</label>
+							</p>
+							<p>
+								<label>
+									<span className="form__labelText">Podaj aktualny kurs euro:</span> <input required className="form__field" type="number" min="0.01" step="0.01" value="4.51" /> </label>
+							</p>
+						</fieldset>
+						<p>
+							<button className="form__button">Oblicz!</button>
+						</p>
+					</form>
+					<div className="form__score">
+						<p><strong className="">N/a</strong> złotych to: <strong className=""> N/a</strong>
+							<span className="">euro.</span>
+						</p>
+					</div>
+				</main>
+			</div>
+
+		</body>
+	)
 }
 
 export default App;
