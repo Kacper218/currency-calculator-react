16d055d98145fad63353f825e561d8c3cfd0a106
Kacper Przybyszewski <kacper.przybyszewskiiii@gmail.com>
2021-05-14 15:42:51 +0200

    Score component added

---
+++ src/Score/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./style.css";
+
+const Score = () => {
+    return (
+
+        <div className="score">
+            <p><strong className="">N/a</strong> złotych to: <strong className=""> N/a</strong>
+                <span className="">euro.</span>
+            </p>
+        </div>
+    );
+};
+
+export default Score;
