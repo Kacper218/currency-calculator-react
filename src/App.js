2cbd7913d31f09c309d1cf107369e7aaad74d70c
Kacper Przybyszewski <kacper.przybyszewskiiii@gmail.com>
2021-05-25 18:24:46 +0200

    options array moved to separate js file

--- src/App.js
+++ src/App.js
@@ -3,26 +3,13 @@ import './App.css';
 import Form from './Form';
 import Score from './Score';
 import Container from './Container';
+import { options } from './options';
 
 function App() {
 
   const [score, setScore] = useState("N/a")
   const [amount, setAmount] = useState(0);
-  const [option, setOption] = useState("");
-
-  const options = [
-    {
-      id: 1,
-      label: "Euro",
-      value: "euro",
-    },
-
-    {
-      id: 2,
-      label: "Magiczna Ruda",
-      value: "ruda",
-    }
-  ];
+  const [option, setOption] = useState("EUR");
 
   return (
     <Container>
---
+++ src/options.js
@@ -0,0 +1,22 @@
+export 
+  const options = [
+    {
+      id: 1,
+      label: "Euro",
+      value: "EUR",
+      converter: 4.51,
+    },
+
+    {
+      id: 2,
+      label: "Magiczna Ruda",
+      value: "RUDY",
+      converter: 21,
+    },
+    {
+      id: 3,
+      label: "Dolar",
+      value: "USD",
+      converter: 3.50,
+    }
+  ];
